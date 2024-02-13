92237a049bbb09e3b39b2af957dc312f526dd1eb
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-13 13:50:44 +0100

    Allow for reverse order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,22 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
-  for (num1; num1 <= num2; num1++){
-    sum += num1;
+  let small = 0;
+  let big = 0;
+
+  if (num1 > num2) {
+    small = num2;
+    big = num1;
+  }
+
+  else {
+    small = num1;
+    big = num2;
   };
+
+  for (small; small <= big; small++){
+    sum += small;
+  };
+
   return parseInt(sum);
 };
 
