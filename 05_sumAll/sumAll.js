14839f4d31f4724e0d7a820d74cf09a0405fa3d8
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-13 13:45:42 +0100

    Add sum function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  for (num1; num1 <= num2; num1++){
+    sum += num1;
+  };
+  return parseInt(sum);
 };
 
 // Do not edit below this line
