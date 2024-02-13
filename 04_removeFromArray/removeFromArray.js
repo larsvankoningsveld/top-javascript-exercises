12274ffe814ab09f472657f751fa276fd26050b4
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-13 13:23:50 +0100

    Add removeArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+  for (const item of items){
+    const found = array.indexOf(item);
+    console.log(`found: ${found}`);
+    array.splice(found, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
