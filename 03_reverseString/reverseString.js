daa344ed5d5356e602ce78ae727df960f399e9a1
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-13 11:46:00 +0100

    Add function that reverses string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  console.log (input);
+  const array = input.split('');
+  console.log(array);
+  const revArray = array.reverse();
+  console.log(revArray);
+  const res = revArray.join('');
+  console.log(res);
+  return res;
 };
 
 // Do not edit below this line
