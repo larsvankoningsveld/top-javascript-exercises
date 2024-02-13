e43341ea5f48411544bb2846ac2c3c9ef1fd4287
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-13 13:31:49 +0100

    Allow for multiple items to be removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
 const removeFromArray = function(array, ...items) {
+
+  
   for (const item of items){
-    const found = array.indexOf(item);
-    console.log(`found: ${found}`);
-    array.splice(found, 1);
-  }
+    while (array.indexOf(item) > -1){
+      const found = array.indexOf(item);
+      console.log(`found: ${found}`);
+      array.splice(found, 1);
+    };
+  };
   return array;
 };
 
