39e0c11e473a9fadecde9646bfffab8bbfaa4568
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-14 15:04:53 +0100

    Add two functionality to convert F to C and C to F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  let res = 0;
+  res = (input - 32) * 5 / 9;
+  res = Math.round(res * 10) / 10;
+  return res;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let res = 0;
+  res = input * 9 / 5 + 32
+  res = Math.round(res * 10) / 10;
+  return res;
 };
 
 // Do not edit below this line
