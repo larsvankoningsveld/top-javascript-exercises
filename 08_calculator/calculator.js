21e8d5d5f695a1733009d502348ecc1a7a3e12c3
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-09-22 21:37:38 +0200

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function(a, b) {
+const add = function (a, b) {
   return a + b;
 };
 
-const subtract = function(a, b) {
+const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function() {
-	
+const sum = function ([...numbers]) {
+  let sum = 0;
+  for (let number of numbers) sum += Number(number)
+  return Number(sum)
 };
 
-const multiply = function() {
+const multiply = function ([...numbers]) {
+  let result = 1;
+  for (let number of numbers) result *= Number(number)
+  return Number(result)
 
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) return 1
+  let result = 1;
+
+  while (number > 0) {
+    result *= number;
+    number--
+  }
+
+  return result
+};
+
+// Recursion example
+const factorialize = function (number) {
+  if (number < 0) {
+    return -1
+  } else if (number == 0) {
+    return 1;
+  } else {
+    return number * factorialize(number - 1)
+  }
 };
 
 // Do not edit below this line
