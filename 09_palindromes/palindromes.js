a45965f6bf43ab54abc4eac0b6d35eeed46fa374
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-09-24 11:04:23 +0200

    Solve palindromes (a little buggy)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const chars = string.toLowerCase().split('')
 
+  const regex = new RegExp('[!?\., ]')
+
+
+  chars.forEach((char, i) => {
+    if (regex.test(char) === true) {
+      chars.splice(i, 1)
+    }
+  })
+
+  chars.forEach((char, i) => {
+    if (regex.test(char) === true) {
+      chars.splice(i, 1)
+    }
+  })
+
+  const reversed = chars.toReversed()
+
+  result = true
+
+  chars.forEach((char, i) => {
+    if (char !== reversed[i]) result = false
+  })
+
+  return result
 };
 
 // Do not edit below this line
